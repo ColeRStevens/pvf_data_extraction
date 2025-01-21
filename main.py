e9c07c9db8399419bb5352879917bd28482d5295
aeonll <aeonification@gmail.com>
2025-01-20 21:52:15 -0600

    CSV output fixed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 from bs4 import BeautifulSoup
 
@@ -12,26 +14,24 @@ teams = ["https://provolleyball.com/teams/indy-ignite/statistics?tab=matchByMatc
         "https://provolleyball.com/teams/san-diego-mojo/statistics?tab=matchByMatch",
         "https://provolleyball.com/teams/vegas-thrill/statistics?tab=matchByMatch"
         ]
-teams.sort()
 def extract_data(url):
 
     xpath_table = '//*[@id="app"]/main/div/div[2]/div/div[2]/div/div/div/table'
     xpath_2024_click = '//*[@id="app"]/main/div/div[2]/div/div[1]/div/select/option[1]'
     xpath_year_switch= '//*[@id="app"]/main/div/div[2]/div/div[1]/div'
-    team_name_xpath = '//*[@id="app"]/main/div/section/div[2]/div/h1'
+
+    name = url[32:].split('/')[0].replace('-', ' ')
+    name = name.title()
 
     driver = webdriver.Firefox()
-    driver.get(teams[0])
-
-    try:
-        element = driver.find_element(By.XPATH, xpath_table)
-        html_element = element.get_attribute('innerHTML')
-        team_name = driver.find_element(By.XPATH, team_name_xpath).text
-        soup = BeautifulSoup(html_element, 'html.parser')
-
-    except:
-         print("path not found")
-         pass
+    driver.get(url)
+    
+   
+    element = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.XPATH, xpath_table)))
+    html_element = element.get_attribute('innerHTML')
+    soup = BeautifulSoup(html_element, 'html.parser')
+    print("2025 data found for ",name)
     
     headers = []
     rows = []
@@ -41,17 +41,36 @@ def extract_data(url):
             headers = [el.text.strip() for el in row.find_all('th')]
         else:
             rows.append([el.text.strip() for el in row.find_all('td')])
+   
+    driver.find_element(By.XPATH,xpath_year_switch).click()
+    driver.find_element(By.XPATH,xpath_2024_click).click()
+    
+    
     try:
-        driver.find_element(By.XPATH,xpath_year_switch).click()
-        driver.find_element(By.XPATH,xpath_2024_click).click()
-        driver.implicitly_wait(2)
-        element = driver.find_element(By.XPATH,xpath_table)
+        element = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.XPATH, xpath_table)))
         html_element = element.get_attribute('innerHTML')
         soup = BeautifulSoup(html_element, 'html.parser')
+        print("2024 data found for",name)
     except:
-        print("path not found")
-        pass
+        print(f"No 2024 data found for",name)
+        for row in rows:
+            value = row[1][:2]
+            if value == "vs":
+                row.insert(2, "Home")
+            elif value == "at":
+                row.insert(2, "Away")
 
+            rows[rows.index(row)][1] = rows[rows.index(row)][1][2:]
+            rows[rows.index(row)][11] = rows[rows.index(row)][11][:-1]
+            rows[rows.index(row)].insert(1, name)
+
+        headers.insert(2,"Location")
+        headers.insert(1,"Team")
+        
+        return rows,headers
+        
+        
     for i, row in enumerate(soup.find_all('tr')):
             if i == 0:
                 headers = [el.text.strip() for el in row.find_all('th')]
@@ -67,24 +86,26 @@ def extract_data(url):
 
             rows[rows.index(row)][1] = rows[rows.index(row)][1][2:]
             rows[rows.index(row)][11] = rows[rows.index(row)][11][:-1]
-            rows[rows.index(row)].insert(1, team_name[:-11])
+            rows[rows.index(row)].insert(1, name)
 
     headers.insert(2,"Location")
     headers.insert(1,"Team")
 
-   
     driver.close()
 
     return rows,headers
 
 def main():
-    rows = []
+    team_rows = []
+    header = []
 
     for team in teams:
-        row,headers = extract_data(team)
-        rows.append(row)
+        rows,headers = extract_data(team)
+        if header == []:
+            header = headers
+        team_rows.extend(rows)
 
-    df = pd.DataFrame(rows, columns=headers)
+    df = pd.DataFrame(team_rows, columns=headers)
     print(df)
     df.to_csv('data/data.csv', index=False)
 
